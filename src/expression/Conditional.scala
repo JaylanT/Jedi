1dc34832bc4ced0c034bb9eeb30e01dd50989225
Jaylan <Jaylan.Tse@gmail.com>
2017-05-01 17:05:59 -0700

    Return Notification("unspecified") if statement is "if (false) ..."

--- src/expression/Conditional.scala
+++ src/expression/Conditional.scala
@@ -1,7 +1,7 @@
 package expression
 
 import system.TypeException
-import value.{Boole, Environment, Value}
+import value.{Boole, Environment, Notification, Value}
 
 /**
   * Created by Jaylan on 4/30/2017.
@@ -9,13 +9,19 @@ import value.{Boole, Environment, Value}
 case class Conditional(exp1: Expression, exp2: Expression, exp3: Option[Expression]) extends SpecialForm {
 
   def execute(env: Environment): Value = {
-    val condition = exp1.execute(env)
-    if (!condition.isInstanceOf[Boole]) throw new TypeException("Input must be a boole")
+    exp1 match {
+      case boole: Boole if !boole.value =>
+        Notification("Unspecified")
+      case _ =>
+        val condition = exp1.execute(env)
+        if (!condition.isInstanceOf[Boole])
+          throw new TypeException("Input must be a boole")
 
-    val boole = condition.asInstanceOf[Boole]
-    if (!boole.value && exp3.isDefined)
-      exp3.get.execute(env)
-    else
-      exp2.execute(env)
+        val boole = condition.asInstanceOf[Boole]
+        if (!boole.value && exp3.isDefined)
+          exp3.get.execute(env)
+        else
+          exp2.execute(env)
+    }
   }
 }
